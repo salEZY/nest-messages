565a101aab49bdcc3cc98d72a1b31a5c8b42ff5a
salEZY <bobicaleks@yahoo.com>
2022-02-26 10:38:01 +0100

    implement repository and service, test with request.http

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,18 +1,27 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message-dto';
+import { MessagesService } from './message.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService
+
+  constructor() {
+    this.messagesService = new MessagesService()
+  }
+
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll()
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    console.log(body.content);
+    return this.messagesService.create(body.content)
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id)
   }
 }
